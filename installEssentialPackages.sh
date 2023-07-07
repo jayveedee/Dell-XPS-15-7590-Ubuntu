1750c814cfe854291c8b39dbfe5e72c354dff418
Jákup Viljam Dam <jayveedees@gmail.com>
2023-07-07 02:01:40 +0100

    Update installEssentialPackages.sh

--- installEssentialPackages.sh
+++ installEssentialPackages.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-sudo apt install nmap tshark tldr gnome-tweaks nome-shell-extensions
+sudo su
+
+# Install some generic packages that are used often
+sudo apt install nmap tshark tldr 
+
+# Install gnome customization packages
+gnome-tweaks nome-shell-extensions
+
+# Install wine and dependencies
+sudo apt install wine winetricks
+sudo dpkg --add-architecture i386 && apt-get update && apt-get install wine32:i386
+
+winetricks directx9
+winetricks vcrun6sp6
+
+# Update/upgrade the system
+sudo apt update
+sudo apt upgrade
